99c6838c68e633fdaf3ec8a8c28a01548dda23b8
yuexin <yuexine@gmail.com>
2018-04-11 19:15:49 +0800

    nothing

--- waffle-common/waffle-common-eureka/src/main/java/com/waffle/common/eureka/EurekaApplication.java
+++ waffle-common/waffle-common-eureka/src/main/java/com/waffle/common/eureka/EurekaApplication.java
@@ -1,7 +1,17 @@
 package com.waffle.common.eureka;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author yuexin
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
 }
