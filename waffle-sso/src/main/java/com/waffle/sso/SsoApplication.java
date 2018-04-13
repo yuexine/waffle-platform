62a0b902ed39e712acc2eff1989b6ff1adba1c2f
yuexin <yuexine@gmail.com>
2018-04-13 21:45:09 +0800

    add some component

---
+++ waffle-sso/src/main/java/com/waffle/sso/SsoApplication.java
@@ -0,0 +1,24 @@
+package com.waffle.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yuexin
+ */
+@RestController
+@SpringBootApplication
+public class SsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoApplication.class, args);
+    }
+
+    @GetMapping
+    public String home() {
+        return "Hello, I am SSO Application";
+    }
+
+}
