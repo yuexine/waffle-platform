bde5c7f3f3da4fc1117b2714e8ae0ecac3193aef
yuexin <yuexine@gmail.com>
2018-06-03 20:55:19 +0800

    模块名称调整

--- waffle-api-blog/src/main/java/com/waffle/api/ApiApplication.java
+++ waffle-api-blog/src/main/java/com/waffle/api/ApiApplication.java
@@ -3,12 +3,14 @@ package com.waffle.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author yuexin
  */
 @EnableHystrix
+@EnableHystrixDashboard
 @EnableFeignClients
 @SpringBootApplication
 public class ApiApplication {
