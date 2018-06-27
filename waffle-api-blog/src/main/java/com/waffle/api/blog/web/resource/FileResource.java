ffb87f990c041be9dcfeed96ae90d08b50795e61
yuexin <yuexine@gmail.com>
2018-06-28 00:24:26 +0800

    add dockerfile

--- waffle-api-blog/src/main/java/com/waffle/api/blog/web/resource/FileResource.java
+++ waffle-api-blog/src/main/java/com/waffle/api/blog/web/resource/FileResource.java
@@ -1,11 +1,13 @@
 package com.waffle.api.blog.web.resource;
 
+import com.waffle.api.blog.constant.CommonConstant;
 import com.waffle.api.blog.service.common.file.FileService;
 import com.waffle.api.blog.service.common.file.MultipartFileResolver;
 import com.waffle.api.blog.web.model.FileDescription;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +18,7 @@ import java.io.IOException;
  * @author yuexin
  */
 @RestController
+@RequestMapping(CommonConstant.PREFIX)
 public class FileResource {
 
     @Autowired
