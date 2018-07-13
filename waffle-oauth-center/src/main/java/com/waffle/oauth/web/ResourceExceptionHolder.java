2b3079b9505e57d5efc642c4c8c52e7de098dc17
yuexin <yuexine@gmail.com>
2018-07-13 14:46:30 +0800

    oauth module add global null point exception handler

---
+++ waffle-oauth-center/src/main/java/com/waffle/oauth/web/ResourceExceptionHolder.java
@@ -0,0 +1,24 @@
+package com.waffle.oauth.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 异常处理
+ *
+ * @author yuexin
+ * @since
+ */
+@Slf4j
+@ControllerAdvice(basePackageClasses = ResourceExceptionHolder.class)
+public class ResourceExceptionHolder {
+
+    @ExceptionHandler({NullPointerException.class})
+    @ResponseBody
+    public String handleNullPointException(NullPointerException exception) {
+        log.error("Oh, No >> {}", exception);
+        return "不走心...";
+    }
+}
