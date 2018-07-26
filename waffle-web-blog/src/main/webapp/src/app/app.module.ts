d0806d79053aab1561b8eca4ad48da02d87d6cc7
TurlingXian <qq476424983@126.com>
2018-07-26 21:46:31 +0800

    安装了material

--- waffle-web-blog/src/main/webapp/src/app/app.module.ts
+++ waffle-web-blog/src/main/webapp/src/app/app.module.ts
@@ -4,13 +4,16 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
